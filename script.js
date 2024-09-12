efe4f0e39c0e9bc1d9a4b9bc5bce8b5cdee3b970
abhishektumula <abhishektumula@gmail.com>
2024-09-12 09:26:40 +0530

    exit button added

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const startBtn = document.querySelector('.start-button');
 const popupinfo = document.querySelector('.poup-info');
+const exitBtn = document.querySelector('.exit');
+const mainBtn = document.querySelector('.clas');
+
 
 startBtn.onclick = () => {
     popupinfo.classList.add('active');
+    mainBtn.classList.add('')
+}
+
+exitBtn.onclick = () => {
+    popupinfo.classList.remove('active');
 }
