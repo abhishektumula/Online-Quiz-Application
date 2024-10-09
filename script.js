1f605083c23f9ddccb881e9c5275d4735ce821d5
abhishektumula <147244373+abhishektumula@users.noreply.github.com>
2024-10-09 14:38:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
-const startBtn = document.querySelector('.start-button');
-const popupinfo = document.querySelector('.poup-info');
-const exitBtn = document.querySelector('.exit');
-const mainBtn = document.querySelector('.clas');
 
-
-startBtn.onclick = () => {
-    popupinfo.classList.add('active');
-    mainBtn.classList.add('')
-}
-
-exitBtn.onclick = () => {
-    popupinfo.classList.remove('active');
-}
